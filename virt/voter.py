de3bbd50bf85668b19897deabc44738ba5cab212
Maxwell Midega <maxwellmidega@gmail.com>
2024-12-20 23:31:08 +0300

    clearing some changes

--- virt/voter.py
+++ virt/voter.py
@@ -2,11 +2,11 @@ from playwright.sync_api import sync_playwright
 import time
 
 # Function to run the script
-def grace_voter():
+def oaey_voter():
     votes = 0
     with sync_playwright() as p:
         # Launch the Chromium browser (set headless=False to see the browser)
-        browser = p.firefox.launch(headless=True)
+        browser = p.webkit.launch(headless=True)
         context = browser.new_context();
         page = context.new_page()
 
@@ -15,23 +15,23 @@ def grace_voter():
             for j in range(5):
                 context.clear_cookies();
 
-                page.goto("https://www.thaiupdate.info/female-star-of-the-year-group-3/")
+                page.goto("https://www.thaiupdate.info/female-star-of-the-year-group-1/")
 
                 # Interact with the page
-                page.locator("id=PDI_answer65709894").check()
-                page.locator("id=pd-vote-button14804257").click()
-                page.wait_for_selector("id=PDI_form14804257", timeout=9000)
+                page.locator("id=PDI_answer65586722").check()
+                page.locator("id=pd-vote-button14773738").click()
+                page.wait_for_selector("id=PDI_form14773738", timeout=9000)
 
                 # Extract and calculate the answer
-                expression = page.locator("id=captcha_14804257").text_content()
+                expression = page.locator("id=captcha_14773738").text_content()
                 numbers = [int(num) for num in expression.split() if num.isdigit()]
                 num1 = numbers[0]
                 num2 = numbers[1]
                 answer = num1 + num2
 
                 # Fill in the answer and click the vote button
-                page.locator("id=answer_14804257").fill(str(answer))
-                page.locator("id=pd-vote-button14804257").click()
+                page.locator("id=answer_14773738").fill(str(answer))
+                page.locator("id=pd-vote-button14773738").click()
 
                 votes += 1
                 print("vote count: ", votes)
@@ -44,4 +44,4 @@ def grace_voter():
         browser.close()
 
 # Run the script
-grace_voter()
+oaey_voter()
